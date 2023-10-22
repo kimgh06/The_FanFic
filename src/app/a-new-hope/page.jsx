c45f44884fe91809e043f8fb4ff4c8921b0329e4
kkh061101 <kkh061101@naver.com>
2023-10-22 18:37:11 +0900

    Error:can be escaped with

--- src/app/a-new-hope/page.jsx
+++ src/app/a-new-hope/page.jsx
@@ -144,13 +144,13 @@ export default function App() {
               A NEW HOPE
             </p>
             <p>
-              It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire.
+              {`It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire.`}
             </p>
             <p>
-              During the battle, rebel spies managed to steal secret plans to the Empire's ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet.
+              {`During the battle, rebel spies managed to steal secret plans to the Empire's ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet.`}
             </p>
             <p>
-              Pursued by the Empire's sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy....
+              {`Pursued by the Empire's sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy....`}
             </p>
           </div>
         </div>
